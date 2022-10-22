a6d59c13b260222b460b307723894e22d8cfe977
willcrystian <willcrystian@gmail.com>
2022-10-22 15:01:48 -0300

    first commit

---
+++ produto/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Produto, Variacao
+
+
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    extra = 1
+    
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display= ('id','nome', 'preco_marketing', 'preco_marketing_promocional', 'slug', 'tipo')
+    list_display_links= ('id', 'nome', 'preco_marketing')
+    inlines = [VariacaoInline]   
+    
+admin.site.register(Produto, ProdutoAdmin)    
+admin.site.register(Variacao)
---
+++ produto/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from PIL import Image
+import os
+from django.conf import settings
+
+class Produto(models.Model):    
+    
+    nome = models.CharField(max_length= 255)
+    descricao_curta = models.TextField(max_length= 255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(upload_to= 'produto_imagens/%Y/%m')
+    slug =models.SlugField(unique= True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default= 0)
+    tipo = models.CharField(default= 'V',
+                            max_length= 1,
+                            choices= (('V', 'Variável'),
+                                           ('S', 'Simples'),
+                                           )
+                            )
+    
+    def __str__(self) -> str:
+        return self.nome
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+    
+        self.resize_image(self.imagem.name, 800)
+    
+    @staticmethod
+    def resize_image(path_image, new_width= 800):
+        image_path = os.path.join(settings.MEDIA_ROOT, path_image)        
+        img = Image.open(image_path)
+        width, heigth = img.size
+        
+        if width <= new_width:
+            img.close()
+            return
+        
+        new_heigth = round((new_width * heigth) / width)
+        new_img = img.resize((new_width, new_heigth), Image.LANCZOS)
+        new_img.save(image_path,
+                     optimize= True,
+                     quality = 60,)
+        img.close()
+        
+
+class Variacao(models.Model):
+    nome = models.CharField(max_length= 255)   
+    produto = models.ForeignKey(Produto, on_delete= models.CASCADE)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+    
+    def __str__(self) -> str:
+        return self.nome or self.produto
+    
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Varições'
+
+    
