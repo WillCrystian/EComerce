1bff8b753f28925af1f92717fbd33e2ce0553cf4
willcrystian <willcrystian@gmail.com>
2022-10-24 16:14:21 -0300

    calculado preço total da compra

--- produto/templatetags/filters.py
+++ produto/templatetags/filters.py
@@ -10,3 +10,7 @@ def estilo_moeda(valor):
 @register.filter
 def cart_total_quant(carrinho):
     return utils.cart_total_quant(carrinho)
+
+@register.filter
+def cart_total_preco(carrinho):
+    return utils.cart_total_preco(carrinho)
--- utils/utils.py
+++ utils/utils.py
@@ -4,3 +4,16 @@ def cart_total_quant(carrinho):
 
 def estilo_moeda(valor):
     return f'R$ {valor:.2f}'.replace('.',',')
+
+def cart_total_preco(carrinho):
+    total_preco = sum(
+        [
+            item.get('preco_quantitativo_promocional')
+            if item.get('preco_quantitativo_promocional')
+            else item.get('preco_quantitativo')
+            for item 
+            in carrinho.values()
+        ]
+    )
+    return round(total_preco, 2)
+    
