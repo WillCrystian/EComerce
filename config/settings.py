bbf5d936c6403809d52333a0f714680535bd3a26
willcrystian <willcrystian@gmail.com>
2022-11-17 09:06:25 -0300

    configurando o head do HTML

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
